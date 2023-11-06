e84e88f5658dc697904e8b6c568c871042e381af
Luz Scarpati <luz.scarpati@gmail.com>
2023-11-06 17:05:32 -0300

    Configuración de handlebars

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 import express from "express";
+import handlebars from "express-handlebars";
+import { __dirname } from "./utils.js";
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
-//import morgan from 'morgan';
+import viewRouter from './routes/views.router.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+// Rutas del cart y de prodcts
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
+
+//Configuración de handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use('/', viewRouter);
 
 const PORT = 8080;
 
